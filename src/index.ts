06520c6995d1de1fac317197123bfc65f8555013
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-12-11 19:31:52 -0300

    mongodb setup

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 import * as compression from 'compression';
 import * as cors from 'cors';
 import * as dotenv from 'dotenv';
+import * as mongoose from 'mongoose';
 
 // Criando app e prepanado para uso
 const app = express();
@@ -30,3 +31,9 @@ server.listen(8080, () => {
 dotenv.config();
 const user = process.env.USERNAME;
 const pass = process.env.PASSWORD;
+const MONGO_URL = `mongodb+srv://${user}:${pass}@cluster0.8u7ji4l.mongodb.net/?retryWrites=true&w=majority`;
+
+// Conexão com MongoDB
+mongoose.Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
