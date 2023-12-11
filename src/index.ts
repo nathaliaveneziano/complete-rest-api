f24d106e8b1636437a8ebbd7b1ee907a716923d7
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-12-11 19:31:01 -0300

    adding env in project

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 import * as compression from 'compression';
 import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 
+// Criando app e prepanado para uso
 const app = express();
 
 app.use(
@@ -19,6 +21,12 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
+// Criando servidor
 server.listen(8080, () => {
   console.log('Server running on http://localhost:8080');
 });
+
+// Configuração do ENV
+dotenv.config();
+const user = process.env.USERNAME;
+const pass = process.env.PASSWORD;
