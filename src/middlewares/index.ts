18eb23c408a97229903afd3d6106ff41104cf561
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-12-14 17:58:23 -0300

    creating users controllers and routing

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { merge } from 'lodash';
+import { get, merge } from 'lodash';
 import { getUserBySessionoken } from '../db/users';
 
 export const isAuthenticated = async (
@@ -25,6 +25,30 @@ export const isAuthenticated = async (
     merge(req, { identity: existingUser });
 
     return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, 'identity._id') as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    next();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
